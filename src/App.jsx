5f518ffc681bed71f9005403b072345da568136a
rashednyn <rashedmahamud22@gmail.com>
2023-09-13 22:35:09 +0600

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   
@@ -7,7 +9,8 @@ function App() {
   return (
     <>
       
-      <h1>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
      
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-bold'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
